e7fdcc3a87ee63b3adafdf371e521d79b0242b93
evanmc21 <emcandler21@gmail.com>
2018-07-31 22:24:55 -0400

    edit page renders properly, still fixing the form though

--- bootylicious-client/src/reducers/workouts.js
+++ bootylicious-client/src/reducers/workouts.js
@@ -10,7 +10,7 @@ export default (state = [], action) => {
       return state;
 
     case 'EDIT_WORKOUT':
-      state.workouts.map((workout) => {
+      state.map((workout) => {
       if (workout.id === action.workout.id) {
         workout.location = action.workout.location
         workout.duration = action.workout.duration
@@ -18,6 +18,8 @@ export default (state = [], action) => {
         workout.reps = action.workout.reps
         workout.muscle_group = action.workout.muscle_group
         workout.weight = action.workout.weight
+        workout.cardio = action.workout.cardio
+        workout.resistance = action.workout.resistance
       }
     })
     return {...state, workout: action.workout};
