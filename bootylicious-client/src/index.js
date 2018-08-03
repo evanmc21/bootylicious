7199610cc831a1cc99497aa2a1027d5012e842ca
evanmc21 <emcandler21@gmail.com>
2018-08-03 13:27:29 -0400

    fixed nav bar and changed styling

--- bootylicious-client/src/index.js
+++ bootylicious-client/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
@@ -8,7 +9,9 @@ import store from './store.js'
 
 ReactDOM.render(
   <Provider store={store}>
+  <BrowserRouter>
   <App />
+  </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
