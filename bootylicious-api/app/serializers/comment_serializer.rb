49bc139004c24fe10d35333d564ba108132d74d7
evanmc21 <emcandler21@gmail.com>
2018-08-27 12:55:56 -0400

    removed serializers

--- bootylicious-api/app/serializers/comment_serializer.rb
+++
@@ -1,5 +0,0 @@
-class CommentSerializer < ActiveModel::Serializer
-attributes :id, :content, :workout_id
-belongs_to :concert
-
-end
--- bootylicious-api/app/serializers/workout_serializer.rb
+++
@@ -1,3 +0,0 @@
-class WorkoutSerializer < ActiveModel::Serializer
-  :location, :muscle_group, :duration, :reps, :sets, :weight, :cardio, :resistance, :count
-end
