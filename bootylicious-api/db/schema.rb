6c21513792e04670cf6120b5b61e5b4156f5db1e
github username <github email address>
2018-08-27 12:54:28 -0400

    removed cardio and resistance columns from workouts

--- bootylicious-api/db/schema.rb
+++ bootylicious-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_150103) do
+ActiveRecord::Schema.define(version: 2018_08_27_165233) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,6 @@ ActiveRecord::Schema.define(version: 2018_08_27_150103) do
     t.integer "sets"
     t.integer "reps"
     t.integer "weight"
-    t.boolean "cardio"
-    t.boolean "resistance"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "like"
