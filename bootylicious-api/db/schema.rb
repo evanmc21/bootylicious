f0503240b3a27c5bf8a5033e56f397a2134b1a98
evanmc21 <emcandler21@gmail.com>
2018-08-27 17:16:00 -0400

    removed count column from workouts

--- bootylicious-api/db/schema.rb
+++ bootylicious-api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_27_165233) do
+ActiveRecord::Schema.define(version: 2018_08_27_210610) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 2018_08_27_165233) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "like"
-    t.integer "count", default: 0
     t.string "note"
     t.date "date"
     t.string "exercise"
